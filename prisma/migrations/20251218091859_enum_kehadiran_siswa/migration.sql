d489d2e0277b34d384aa79a3a27658cce05c5220
Anzalas <anzalas.muhammad@gmail.com>
2025-12-18 22:10:19 +0700

    18 des 2.1

--- prisma/migrations/20251218091859_enum_kehadiran_siswa/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `keterangan` on table `KehadiranSiswa` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "KehadiranSiswa" ALTER COLUMN "keterangan" SET NOT NULL;
